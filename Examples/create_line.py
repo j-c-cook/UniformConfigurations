da5da734d6e31cd725401815a0159bafa7f39e7c
j-c-cook <jack.cook@okstate.edu>
2020-07-01 23:39:23 -0500

    add in examples

--- Examples/create_line.py
+++ Examples/create_line.py
@@ -1,2 +1,26 @@
 # Jack C. Cook
 # 7/1/20
+
+# Creating borehole fields in a line
+
+import Create_Rectangular_Fields as fieldgen
+
+
+def main():
+    # Four boreholes in a line
+    # -------- inputs ----------
+    BottomY = 1
+    LeftX = 4
+    SpaceX = 5
+    DistanceX = (LeftX - 1) * SpaceX
+    # --------------------------
+
+    fgen = fieldgen.Gen_Field.FieldGenerator(BottomY=BottomY, LeftX=LeftX, SpaceX=SpaceX,
+                                             DistanceX=DistanceX, Name='line')
+    print(fgen.borehole_locations)
+    fgen.__display_field__(show_plot=True)
+    fgen.__export_field__()
+
+
+if __name__ == '__main__':
+    main()
